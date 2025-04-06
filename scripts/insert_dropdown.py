04734ae7ba0600336598fc9a25b6bdbaf5b5b9b6
Luis <ge34lah@mytum.de>
2025-04-06 12:25:04 +0200

    script works without anchors for ipynb's and md's

--- scripts/insert_dropdown.py
+++ scripts/insert_dropdown.py
@@ -8,7 +8,7 @@ from keytakeaways import Key_takeaways
 
 black_list_directories = ["_build", "_static", "src"]
 black_list_files_yml = ["prior_art", "scrna_seq", "introduction", "muon_to_seurat"]
-all_files = [
+black_list_files_lamin = [
     # IPYNB files
     "gene_regulatory_networks_atac",
     "quality_control",
@@ -49,6 +49,70 @@ all_files = [
     "raw_data_processing",
 ]
 
+with open("scripts/env_setup.md") as f:
+    md_env_setup = f.read()
+
+with open("scripts/lamin_setup.md") as f:
+    md_lamin_setup = f.read()
+
+
+def get_dropdowns_str(notebook_path: Path) -> list[str]:
+    dropdowns_str = ""
+    dropdowns_str += _get_key_takeaways_str(notebook_path)
+    dropdowns_str += _get_env_setup_str(notebook_path)
+    dropdowns_str += _get_lamin_setup_str(notebook_path)
+    return dropdowns_str
+
+
+def insert_dropdowns_in_lines(
+    lines: list[str], index_title: int, notebook_path: Path
+) -> None:
+    if "\n" not in lines[index_title]:
+        lines[index_title] += "\n"
+
+    index_anchor_dropdown = _get_index_in_cell("<!-- END DROPDOWNS -->\n", lines)
+    if index_anchor_dropdown is not None:
+        print("test")
+        del lines[index_title + 1 : index_anchor_dropdown]
+    else:
+        lines.insert(index_title + 1, "<!-- END DROPDOWNS -->\n")
+
+    lines.insert(index_title + 1, get_dropdowns_str(notebook_path))
+
+
+def _get_lamin_setup_str(notebook_path: Path) -> str:
+    name_yml_file = os.path.split(notebook_path)[1].split(".")[0]
+
+    # replace notebook yml with section yml if yml_file doesn't exist
+    if name_yml_file not in black_list_files_lamin:
+        return md_lamin_setup
+    else:
+        return ""
+
+
+def _get_env_setup_str(notebook_path: Path) -> str:
+    nb_path_folder = os.path.split(notebook_path)[0]
+    nb_path_file = os.path.split(notebook_path)[1]
+    yml_file = nb_path_file.split(".")[0] + ".yml"
+
+    # replace notebook yml with section yml if yml_file doesn't exist
+    if not (Path(nb_path_folder) / yml_file).is_file():
+        yml_file = nb_path_folder.split("/")[-1] + ".yml"
+
+    return md_env_setup.replace("?yml_file_path?", yml_file)
+
+
+def _get_key_takeaways_str(notebook_path: Path) -> str:
+    nb_path_folder = os.path.split(notebook_path)[0]
+    nb_path_file = os.path.split(notebook_path)[1]
+    keytakeaways_file = nb_path_file.split(".")[0] + ".keytakeaways"
+    keytakeaways_path = Path(nb_path_folder) / keytakeaways_file
+    if not keytakeaways_path.is_file():
+        return ""
+    else:
+        keytakeaways_cur = Key_takeaways(keytakeaways_path)
+        return keytakeaways_cur.get_key_takeaway_dropdown_str()
+
 
 def insert_to_ipynb(notebook_path: Path, n_cells: int) -> dict[str, list | dict | int]:
     try:
@@ -68,55 +132,17 @@ def insert_to_ipynb(notebook_path: Path, n_cells: int) -> dict[str, list | dict 
     for cell in nb["cells"]:
         if cell["cell_type"] == "markdown":
             # I will replace this redundant code block with a function if we stick to the anchor approach
-            index_start_env = _get_index_in_cell(
-                "<!-- START ENV-SETUP -->\n", cell["source"]
-            )
-            index_end_env = _get_index_in_cell(
-                "<!-- END ENV-SETUP -->\n", cell["source"]
-            )
 
-            anchor_found = False
-            if index_start_env is not None and index_end_env is not None:
-                del cell["source"][index_start_env + 1 : index_end_env]
-                cell["source"][index_start_env + 1 : index_start_env] = [
-                    _get_env_setup_str(notebook_path, md_env_setup)
-                ]
-                anchor_found = True
-
-            index_start_lamin = _get_index_in_cell(
-                "<!-- START LAMIN-SETUP -->\n", cell["source"]
+            index_title = next(
+                (
+                    index
+                    for index, line in enumerate(cell["source"])
+                    if line.startswith("#")
+                ),
+                None,
             )
-            index_end_lamin = _get_index_in_cell(
-                "<!-- END LAMIN-SETUP -->\n", cell["source"]
-            )
-
-            if index_start_lamin is not None and index_end_lamin is not None:
-                del cell["source"][index_start_lamin + 1 : index_end_lamin]
-                cell["source"][index_start_lamin + 1 : index_start_lamin] = [
-                    md_lamin_setup
-                ]
-                anchor_found = True
-
-            index_start_key_takeaways = _get_index_in_cell(
-                "<!-- START KEY-TAKEAWAYS -->\n", cell["source"]
-            )
-            index_end_key_takeaways = _get_index_in_cell(
-                "<!-- END KEY-TAKEAWAYS -->\n", cell["source"]
-            )
-
-            if (
-                index_start_key_takeaways is not None
-                and index_end_key_takeaways is not None
-            ):
-                del cell["source"][
-                    index_start_key_takeaways + 1 : index_end_key_takeaways
-                ]
-                cell["source"][
-                    index_start_key_takeaways + 1 : index_start_key_takeaways
-                ] = [_get_key_takeaways_str(notebook_path)]
-                anchor_found = True
-
-            if anchor_found:
+            if index_title is not None:
+                insert_dropdowns_in_lines(cell["source"], index_title, notebook_path)
                 return nb
 
         n_cells_checked += 1
@@ -131,6 +157,8 @@ def insert_to_md(md_path: Path, n_lines: int) -> None:
     try:
         with open(md_path, encoding="utf-8") as f:
             content = f.readlines()
+            # print(type(content))
+            # print(content)
     except FileNotFoundError:
         logging.error(f"File not found: {md_path}", exc_info=True)
         raise
@@ -138,12 +166,26 @@ def insert_to_md(md_path: Path, n_lines: int) -> None:
     if not content:
         raise RuntimeError(f"Markdown file {md_path} is empty.")
 
+    # n_lines_to_check_for_anchor =
     n_lines_checked = 0
     # Check if in the first n lines there is a markdown cell with the anchors
     # "<!-- START ENV-SETUP -->" and "<!-- END ENV-SETUP -->". If so, we replace
     # the content between the anchors with the newly loaded dropdown string
     # and write the file.
     for line_number, line in enumerate(content):
+        if line.startswith("# "):
+            index_title = line_number
+            insert_dropdowns_in_lines(content, index_title, md_path)
+
+            with open(md_path, "w", encoding="utf-8") as f:
+                f.writelines(content)
+
+            return
+
+        n_lines_checked += 1
+        if n_lines_checked >= n_lines:
+            break
+
         if "<!-- START ENV-SETUP -->" in line:
             index_start = line_number
             index_end = None
@@ -157,7 +199,7 @@ def insert_to_md(md_path: Path, n_lines: int) -> None:
 
             if index_start is not None and index_end is not None:
                 # Replace the content between the anchors
-                env_setup_str = _get_env_setup_str(md_path, md_env_setup)
+                env_setup_str = _get_env_setup_str(md_path)
                 new_content = (
                     content[: index_start + 1]
                     + [env_setup_str + "\n"]
@@ -181,64 +223,21 @@ def _get_index_in_cell(str_anchor: str, cell_content: Sequence[str]) -> int | No
         return None
 
 
-def _get_env_setup_str(notebook_path: Path, md_env_setup: str) -> str:
-    nb_path_folder = os.path.split(notebook_path)[0]
-    nb_path_file = os.path.split(notebook_path)[1]
-    yml_file = nb_path_file.split(".")[0] + ".yml"
+def main():
+    # insert env dropdown to all .ipynb's
+    notebooks_ipynb = Path("jupyter-book").glob("**/*.ipynb")
 
-    # replace notebook yml with section yml if yml_file doesn't exist
-    if not (Path(nb_path_folder) / yml_file).is_file():
-        yml_file = nb_path_folder.split("/")[-1] + ".yml"
+    # Only insert dropdowns to .ipynb- and .md-files that are in direct subfolders of "jupyter-book" and not in black_list_directories
+    notebooks_ipynb = []
+    notebooks_md = []
+    for subdir in Path("jupyter-book").iterdir():
+        if subdir.is_dir() and all(
+            excluded not in str(subdir) for excluded in black_list_directories
+        ):
+            notebooks_ipynb.extend(subdir.glob("*.ipynb"))
+            notebooks_md.extend(subdir.glob("*.md"))
 
-    return md_env_setup.replace("?yml_file_path?", yml_file)
-
-
-def _get_key_takeaways_str(notebook_path: Path) -> str:
-    nb_path_folder = os.path.split(notebook_path)[0]
-    nb_path_file = os.path.split(notebook_path)[1]
-    keytakeaways_file = nb_path_file.split(".")[0] + ".keytakeaways"
-    keytakeaways_path = Path(nb_path_folder) / keytakeaways_file
-    if not keytakeaways_path.is_file():
-        return "<!-- " + keytakeaways_file + " DOES NOT EXIST -->"
-    else:
-        keytakeaways_cur = Key_takeaways(keytakeaways_path)
-        return keytakeaways_cur.get_key_takeaway_dropdown_str()
-
-
-with open("scripts/env_setup.md") as f:
-    md_env_setup = f.read()
-
-with open("scripts/lamin_setup.md") as f:
-    md_lamin_setup = f.read()
-
-# insert env dropdown to all .ipynb's
-notebooks_ipynb = Path("jupyter-book").glob("**/*.ipynb")
-
-# Only .ipynb files in direct subfolders of "jupyter-book"
-notebooks_ipynb = []
-notebooks_md = []
-
-for subdir in Path("jupyter-book").iterdir():
-    if subdir.is_dir() and all(
-        excluded not in str(subdir) for excluded in black_list_directories
-    ):
-        notebooks_ipynb.extend(subdir.glob("*.ipynb"))
-        notebooks_md.extend(subdir.glob("*.md"))
-
-        # Only consider directories (not files)
-        # notebooks_ipynb.extend(subdir.glob("*.ipynb"))  # Find all .ipynb files in the subfolder
-
-# Print aller Pfade (formatiert)
-# print("IPYNB-Files:")
-# for path in notebooks_ipynb:
-#     print(f"  - {path}")
-
-# print("\nMD-Files:")
-# for path in notebooks_md:
-#     print(f"  - {path}")
-
-for notebook in notebooks_ipynb:
-    if "_build" not in str(notebook):
+    for notebook in notebooks_ipynb:
         nb = insert_to_ipynb(notebook, 5)
 
         if nb is not None:
@@ -247,7 +246,55 @@ for notebook in notebooks_ipynb:
         else:
             raise RuntimeError(f"Failed to process notebook: {notebook}")
 
-# insert env dropdown to all .md's
-notebooks_md = Path("jupyter-book/introduction").glob("**/*.md")
-for notebook in notebooks_md:
-    insert_to_md(notebook, 100)
+    for notebook in notebooks_md:
+        insert_to_md(notebook, 100)
+
+
+if __name__ == "__main__":
+    main()
+else:
+    with open("scripts/env_setup.md") as f:
+        md_env_setup = f.read()
+
+    with open("scripts/lamin_setup.md") as f:
+        md_lamin_setup = f.read()
+
+    # insert env dropdown to all .ipynb's
+    notebooks_ipynb = Path("jupyter-book").glob("**/*.ipynb")
+
+    # Only .ipynb files in direct subfolders of "jupyter-book"
+    notebooks_ipynb = []
+    notebooks_md = []
+
+    for subdir in Path("jupyter-book").iterdir():
+        if subdir.is_dir() and all(
+            excluded not in str(subdir) for excluded in black_list_directories
+        ):
+            notebooks_ipynb.extend(subdir.glob("*.ipynb"))
+            notebooks_md.extend(subdir.glob("*.md"))
+
+            # Only consider directories (not files)
+            # notebooks_ipynb.extend(subdir.glob("*.ipynb"))  # Find all .ipynb files in the subfolder
+
+    # Print aller Pfade (formatiert)
+    # print("IPYNB-Files:")
+    # for path in notebooks_ipynb:
+    #     print(f"  - {path}")
+
+    # print("\nMD-Files:")
+    # for path in notebooks_md:
+    #     print(f"  - {path}")
+
+    for notebook in notebooks_ipynb:
+        nb = insert_to_ipynb(notebook, 5)
+
+        if nb is not None:
+            with open(notebook, "w", encoding="utf-8") as f:
+                json.dump(nb, f, indent=2)
+        else:
+            raise RuntimeError(f"Failed to process notebook: {notebook}")
+
+    # insert env dropdown to all .md's
+    notebooks_md = Path("jupyter-book/introduction").glob("**/*.md")
+    for notebook in notebooks_md:
+        md = insert_to_md(notebook, 100)
